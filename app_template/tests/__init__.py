b2d5a1adc0fb5a9d34465eb86daeac7dbce78053
aodin <aaronoellis@gmail.com>
2013-12-09 11:40:50 -0800

    Subdirectories for tests were removed in Django 1.6

--- app_template/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*
-
-# TODO each test file must be imported separately
-# Would creating a test suite solve this? http://djangosnippets.org/snippets/1972/
-from {{ app_name }}.tests.tests import *
